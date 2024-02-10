bdcd53dda326b1033be7583134e3dc98f36e4e54
YesyBenitez99 <47905130+YesyBenitez99@users.noreply.github.com>
2024-02-10 15:49:42 -0600

    Add files via upload

---
+++ lab.js
@@ -0,0 +1,22 @@
+// ejemplo 1
+const nombre = document.querySelector(".nombre");
+const btn = document.querySelector("button");
+
+btn.addEventListener("Click", CambiaColor);
+
+function CambiaColor(){
+    nombre.style.color ="pink";
+}
+
+//ejemplo 2
+
+const cli = document.querySelector("cli");
+cli.addEventListener("Click", mifun);
+
+function mifun(){
+    alert('Se ha dado click al boton!');
+}
+
+
+// ejemplo 3
+
